3edeecc8b685ab0f55150ad17b872b450c84e855
Th3At0nic <islammdrahatul@gmail.com>
2025-01-23 19:06:55 +0600

    done: added custom error handler to global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,11 @@
 import { ErrorRequestHandler } from 'express';
 import config from '../config';
 import { TErrorSource } from '../interface/error';
+import { ZodError } from 'zod';
+import { handleZodError } from '../utils/error/handleZodError';
+import { handleMongooseError } from '../utils/error/handleMongooseError';
+import { handleCastError } from '../utils/error/handleCastError';
+import { handleDuplicateError } from '../utils/error/handleDuplicateError';
 
 export const globalErrorHandler: ErrorRequestHandler = (
   err,
@@ -10,11 +15,40 @@ export const globalErrorHandler: ErrorRequestHandler = (
   res,
   next,
 ) => {
+  let statusCode = err.statusCode || 500;
   const success = err.success;
-  const errorSource: TErrorSource = err.errorSource;
-  const message = err.message;
+  let errorSource: TErrorSource = err.errorSource || [
+    {
+      path: '',
+      message: err.message || 'Something went wrong',
+    },
+  ];
 
-  res.status(500).json({
+  let message = err.message || 'Something went wrong';
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSource;
+  } else if (err.name === 'ValidationError') {
+    const simplifiedError = handleMongooseError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSource;
+  } else if (err.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSource;
+  } else if (err.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSource = simplifiedError.errorSource;
+  }
+
+  res.status(statusCode).json({
     success: success,
     message: message,
     errorSource: errorSource,
