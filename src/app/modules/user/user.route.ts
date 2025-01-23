7a79c2f93cebff63abe7c4e68a8f96413d6c2626
Th3At0nic <islammdrahatul@gmail.com>
2025-01-23 23:45:27 +0600

    done: user validation by middleware validateRequest

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { UserControllers } from './user.controller';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { UserValidationSchema } from './user.validation';
 
 const router = Router();
 
-router.post('/register', UserControllers.registerUser);
+router.post(
+  '/register',
+  validateRequest(UserValidationSchema),
+  UserControllers.registerUser,
+);
 
 export const UserRoutes = router;
