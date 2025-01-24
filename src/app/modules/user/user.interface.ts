2da77d32e54f0b8e241577d3c2340878a4851e5b
Th3At0nic <islammdrahatul@gmai.com>
2025-01-24 11:20:11 +0600

    done:1.2login user, module(auth,user), login, generate token

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+import { USER_ROLE } from './user.constant';
+
 export type TUser = {
   name: string; // The full name of the user
   email: string; // The email address used for authentication and communication
@@ -5,3 +9,13 @@ export type TUser = {
   role: 'admin' | 'user'; // The role determining access level, default is "user"
   isBlocked: boolean; // Indicates whether the user is blocked, default is false
 };
+
+export interface IUser extends Model<TUser> {
+  isUserExists(id: string): Promise<TUser | null>;
+  isPasswordCorrect(
+    plainTextPassword: string,
+    hashPassword: string,
+  ): Promise<boolean>;
+}
+
+export type TUserRole = keyof typeof USER_ROLE;
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,17 @@
-import { Router } from 'express';
-import { UserControllers } from './user.controller';
-import { validateRequest } from '../../middlewares/validateRequest';
-import { UserValidationSchema } from './user.validation';
+// import { Router } from 'express';
+// import { UserControllers } from './user.controller';
+// import { validateRequest } from '../../middlewares/validateRequest';
+// import { UserValidationSchema } from './user.validation';
 
-const router = Router();
+// const router = Router();
 
-router.post(
-  '/register',
-  validateRequest(UserValidationSchema),
-  UserControllers.registerUser,
-);
+// router.post(
+//   '/register',
+//   validateRequest(UserValidationSchema),
+//   UserControllers.registerUser,
+// );
 
-export const UserRoutes = router;
+// export const UserRoutes = router;
+
+
+// register route is moved to auth.route.ts
