e823280502f5bb9b9721f48023b0e60a2b271873
Th3At0nic <islammdrahatul@gmail.com>
2025-01-25 11:53:37 +0600

    done: blog delete by admin, all business logic done

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import throwAppError from '../../utils/throwAppError';
 import { UserModel } from '../user/user.model';
+import { BlogModel } from '../blog/blog.model';
 
 const blockUserByAdminIntoDB = async (userId: string) => {
   const isUserExists = await UserModel.findById(userId);
@@ -21,7 +22,11 @@ const blockUserByAdminIntoDB = async (userId: string) => {
     );
   }
 
-  const result = await UserModel.findByIdAndUpdate(userId, { isBlocked: true });
+  const result = await UserModel.findByIdAndUpdate(
+    userId,
+    { isBlocked: true },
+    { new: true, runValidators: true },
+  );
 
   if (!result) {
     throwAppError(
@@ -34,6 +39,41 @@ const blockUserByAdminIntoDB = async (userId: string) => {
   return result;
 };
 
+const deleteBlogByAdminIntoDB = async (blogId: string) => {
+  const isBlogExists = await BlogModel.findById(blogId);
+
+  if (!isBlogExists) {
+    throwAppError(
+      'params.id',
+      `Blog not found with the id: ${blogId}`,
+      StatusCodes.NOT_FOUND,
+    );
+  }
+
+  //this block of code is not given as assignment,
+  // i did it as a choice as it does not conflict with assignment, but a extra feature might be used in future
+  if (!isBlogExists?.isPublished) {
+    throwAppError(
+      'params.id',
+      `Blog with the id: ${blogId} is not published. So not deleted`,
+      StatusCodes.FORBIDDEN,
+    );
+  }
+
+  const result = await BlogModel.findByIdAndDelete(blogId);
+
+  if (!result) {
+    throwAppError(
+      'params.id',
+      `Failed to delete the blog with id: ${blogId}`,
+      StatusCodes.INTERNAL_SERVER_ERROR, // If the update fails, server error
+    );
+  }
+
+  return result;
+};
+
 export const AdminServices = {
   blockUserByAdminIntoDB,
+  deleteBlogByAdminIntoDB,
 };
